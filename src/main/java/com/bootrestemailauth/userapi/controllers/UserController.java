2db1b5b5fb5d9b2831443e7e8e7e9e95eacd1d2a
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-02-07 19:35:18 +0530

    User Controller

--- src/main/java/com/bootrestemailauth/userapi/controllers/UserController.java
+++ src/main/java/com/bootrestemailauth/userapi/controllers/UserController.java
@@ -1,16 +1,25 @@
 package com.bootrestemailauth.userapi.controllers;
 
+import java.net.http.HttpHeaders;
+import java.sql.Blob;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
 import com.bootrestemailauth.userapi.dao.UserDao;
+import com.bootrestemailauth.userapi.entities.BlobResponse;
 import com.bootrestemailauth.userapi.entities.JwtRequest;
+import com.bootrestemailauth.userapi.helper.JwtUtil;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -24,6 +33,13 @@ public class UserController {
 
     @Autowired
     public JwtRequest jwtRequest;
+
+    @Autowired
+    public JwtUtil jwtUtil;
+
+    @Autowired
+    public BlobResponse blobResponse;
+
     
     @GetMapping("/users")
     public ResponseEntity<List<JwtRequest>> getUsers(){
@@ -44,6 +60,7 @@ public class UserController {
     @GetMapping("/users/{useremail}")
     public ResponseEntity<?> getUser(@PathVariable("useremail") String useremail){
         
+
         try {
 
             jwtRequest = userDao.getJwtRequestByuseremail(useremail);
@@ -56,6 +73,33 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
+    }
+
+    //New function try this image return karne ke liye hua toh god
+    @GetMapping("/profile-image")
+
+    public ResponseEntity<?> getProfileImage(  @RequestHeader("Authorization") String authorization){
+
+        try{
+            
+            String jwtToken = authorization.substring(7);
+            String email = jwtUtil.extractUsername(jwtToken);
+            jwtRequest = userDao.getJwtRequestByuseremail(email);
+            int blobLength = (int) jwtRequest.getImg().length();
+            byte[] blobAsBytes = jwtRequest.getImg().getBytes(1, blobLength);
+            String s = new String(blobAsBytes);
+            blobResponse.setMessage("Successfull");
+            blobResponse.setProfile_image(s);
+            System.out.println(s);
+            return ResponseEntity.ok(blobResponse);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            blobResponse.setMessage(e.getMessage());
+            blobResponse.setProfile_image("");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(blobResponse);
+        }
+        
     }
 
     
