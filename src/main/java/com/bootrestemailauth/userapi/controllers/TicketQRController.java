f642661e16cda308a14cb9dbd8d5ece1f9ba6e02
Shraddha Mulay <76682198+shraddhamulay09@users.noreply.github.com>
2022-03-13 13:33:11 +0530

    Update TicketQRController.java

--- src/main/java/com/bootrestemailauth/userapi/controllers/TicketQRController.java
+++ src/main/java/com/bootrestemailauth/userapi/controllers/TicketQRController.java
@@ -19,12 +19,12 @@ public class TicketQRController {
     public TicketQRService ticketQRService;
 
     @PostMapping("/addQRticket")
-    public ResponseEntity<?> addQRticket(@RequestHeader("Authorization") String authorization, @RequestParam("monument_name") String monument_name, @RequestParam("no_of_tickets") int no_of_tickets, @RequestParam("fare") double fare,@RequestParam("indian_adult")int indian_adult,@RequestParam("indian_child")int indian_child,@RequestParam("foreign_adult")int foreign_adult,@RequestParam("foreign_child")int foreign_child,@RequestParam("males")int males,@RequestParam("females")int females,@RequestParam("date_of_visit")Date date_of_visit){
+    public ResponseEntity<?> addQRticket(@RequestHeader("Authorization") String authorization, @RequestParam("monument_name") String monument_name, @RequestParam("no_of_tickets") int no_of_tickets, @RequestParam("fare") double fare,@RequestParam("indian_adult")int indian_adult,@RequestParam("indian_child")int indian_child,@RequestParam("foreign_adult")int foreign_adult,@RequestParam("foreign_child")int foreign_child,@RequestParam("males")int males,@RequestParam("females")int females,@RequestParam("date_of_visit")String date_of_visit){
         return ticketQRService.addTicketQR(authorization, monument_name, no_of_tickets, fare, indian_adult, indian_child, foreign_adult, foreign_child, males, females, date_of_visit);
     }
 
     @DeleteMapping("/removeQRticket")
-    public ResponseEntity<?> removeQRticket(@RequestHeader("Authorization") String authorization,@RequestParam("monument_name") String monument_name,@RequestParam("date_of_visit")Date date_of_visit){
+    public ResponseEntity<?> removeQRticket(@RequestHeader("Authorization") String authorization,@RequestParam("monument_name") String monument_name,@RequestParam("date_of_visit")String date_of_visit){
         
         return ticketQRService.removeQRticket(authorization, monument_name, date_of_visit);
     }
